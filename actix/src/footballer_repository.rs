64b3b2b5d1f9c7c672e98c617fe4093f0948e798
Christian Schaible <christian.schaible@novatec-gmbh.de>
2020-06-28 13:54:39 +0200

    Run modifying operations transactional, adjust response codes and run migrations on startup in the actix implementation

--- actix/src/footballer_repository.rs
+++ actix/src/footballer_repository.rs
@@ -1,9 +1,8 @@
-use std::{ops::Deref, sync::Arc};
+use std::sync::Arc;
 
 use diesel::prelude::*;
 use diesel::r2d2::ConnectionManager;
 use diesel::r2d2::Pool;
-use diesel::result::Error;
 use diesel::PgConnection;
 
 use crate::footballer::Footballer;
@@ -22,49 +21,106 @@ impl FootballerRepository {
     pub fn create(&self, v_footballer: NewFootballer) -> Result<Footballer, Error> {
         use crate::schema::footballer::dsl::*;
 
-        match diesel::insert_into(footballer)
-            .values(&v_footballer)
-            .get_result::<Footballer>(self.m_pool.deref().get().unwrap().deref())
-        {
-            Ok(saved_footballer) => Ok(saved_footballer),
-            Err(e) => Err(e.into()),
-        }
+        // Get connection from pool
+        let conn = &*self.m_pool.get()?;
+
+        // Start transaction and save data
+        Ok(conn.transaction::<_, diesel::result::Error, _>(|| {
+            Ok(diesel::insert_into(footballer)
+                .values(&v_footballer)
+                .get_result::<Footballer>(conn)?)
+        })?)
     }
 
     /// Find a footballer by its position
     pub fn find_by_position(&self, footballer_position: &str) -> Result<Vec<Footballer>, Error> {
         use crate::schema::footballer::dsl::*;
 
-        let footballers = footballer
-            .filter(position.eq(footballer_position))
-            .load::<Footballer>(self.m_pool.deref().get().unwrap().deref())?;
+        // Get connection from pool
+        let conn = &*self.m_pool.get()?;
 
-        Ok(footballers)
+        // Start transaction and load data
+        Ok(self.read_only_transaction(conn, || {
+            footballer
+                .filter(position.eq(footballer_position))
+                .load::<Footballer>(conn)
+        })?)
     }
 
     /// Find a footballer by its id
-    pub fn find_by_id(&self, footballer_id: i64) -> Result<Footballer, Error> {
+    pub fn find_by_id(&self, footballer_id: i64) -> Result<Option<Footballer>, Error> {
         use crate::schema::footballer::dsl::*;
 
+        // Get connection from pool
+        let conn = &*self.m_pool.get()?;
+
+        // Load data
         Ok(footballer
             .filter(id.eq(footballer_id))
-            .first::<Footballer>(self.m_pool.deref().get().unwrap().deref())?)
+            .first::<Footballer>(conn)
+            .optional()?)
     }
 
     /// Find all footballers
     pub fn find_all(&self) -> Result<Vec<Footballer>, Error> {
         use crate::schema::footballer::dsl::*;
 
-        Ok(footballer.load::<Footballer>(self.m_pool.deref().get().unwrap().deref())?)
+        // Get connection from pool
+        let conn = &*self.m_pool.get()?;
+
+        // Load data
+        Ok(footballer.load::<Footballer>(conn)?)
     }
 
     /// Delete a footballer by its id
     pub fn delete_by_id(&self, footballer_id: i64) -> Result<bool, Error> {
         use crate::schema::footballer::dsl::*;
 
-        let count = diesel::delete(footballer.filter(id.eq(footballer_id)))
-            .execute(self.m_pool.deref().get().unwrap().deref())?;
+        // Get connection from pool
+        let conn = &*self.m_pool.get()?;
 
+        // Start transaction and delete data
+        let count = conn.transaction::<_, diesel::result::Error, _>(|| {
+            Ok(diesel::delete(footballer.filter(id.eq(footballer_id))).execute(conn)?)
+        })?;
         Ok(count > 0)
+    }
+
+    fn read_only_transaction<T, E, F>(
+        &self,
+        conn: &PgConnection,
+        f: F,
+    ) -> core::result::Result<T, E>
+    where
+        F: FnOnce() -> Result<T, E>,
+        E: From<diesel::result::Error>,
+    {
+        match conn
+            .build_transaction()
+            .read_only()
+            .read_committed()
+            .run::<_, diesel::result::Error, _>(|| Ok(f()))
+        {
+            Ok(result) => result,
+            Err(e) => Err(e.into()),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub enum Error {
+    DieselError(diesel::result::Error),
+    NoDbConnectionError(r2d2::Error),
+}
+
+impl From<diesel::result::Error> for Error {
+    fn from(e: diesel::result::Error) -> Self {
+        Self::DieselError(e)
+    }
+}
+
+impl From<r2d2::Error> for Error {
+    fn from(e: r2d2::Error) -> Self {
+        Self::NoDbConnectionError(e)
     }
 }
--- actix/src/main.rs
+++ actix/src/main.rs
@@ -1,35 +1,54 @@
 #[macro_use]
 extern crate diesel;
 #[macro_use]
+extern crate diesel_migrations;
 extern crate log;
 
 use std::sync::Arc;
 
-use actix_web::{middleware, App, HttpServer};
+use actix_web::error::BlockingError;
+use actix_web::{middleware, web, App, HttpServer};
+use anyhow::format_err;
 use diesel::r2d2::ConnectionManager;
 use diesel::PgConnection;
+use log::LevelFilter;
+use log::{error, warn};
 use r2d2::Pool;
 
 use crate::footballer_repository::FootballerRepository;
-use log::LevelFilter;
 
 pub mod footballer;
 pub mod footballer_repository;
 pub mod footballer_rest_controller;
 pub mod schema;
 
+embed_migrations!();
+
+async fn run_db_migrations(
+    pool: Arc<Pool<ConnectionManager<PgConnection>>>,
+) -> Result<(), BlockingError<anyhow::Error>> {
+    web::block(move || {
+        warn!("Migrate Database");
+        embedded_migrations::run_with_output(&*pool.get()?, &mut std::io::stdout())?;
+        Ok(())
+    })
+    .await
+}
+
 #[actix_rt::main]
-async fn main() -> std::io::Result<()> {
+async fn main() -> anyhow::Result<()> {
     dotenv::dotenv().ok();
-    match std::env::var("RUST_LOG") {
-        Err(_) => std::env::set_var("RUST_LOG", "warn"),
-        _ => {}
-    };
+
+    // Configure logging
+    if std::env::var("RUST_LOG").is_err() {
+        std::env::set_var("RUST_LOG", "warn")
+    }
 
     env_logger::builder()
         .filter(Some("actix_server::builder"), LevelFilter::Info)
         .init();
 
+    // Configure database and migrate
     let database_url = std::env::var("DATABASE_URL").unwrap_or_else(|_e| {
         error!("DATABASE_URL environment variable not set");
         std::process::exit(1);
@@ -37,8 +56,15 @@ async fn main() -> std::io::Result<()> {
 
     let pool = Arc::new(init_pool(database_url.as_str()));
 
+    run_db_migrations(pool.clone())
+        .await
+        .err()
+        .map(|e| format_err!("Database migration failed with error: {}", e));
+
+    // Configure repository
     let repository = Arc::new(FootballerRepository::new(pool.clone()));
 
+    // Start the web server
     HttpServer::new(move || {
         App::new()
             .data(repository.clone())
@@ -48,17 +74,18 @@ async fn main() -> std::io::Result<()> {
             .service(footballer_rest_controller::footballer_get)
             .service(footballer_rest_controller::footballer_search)
     })
-        .bind(("0.0.0.0", 8080))?
-        .run()
-        .await
+    .bind(("0.0.0.0", 8080))?
+    .run()
+    .await?;
+
+    Ok(())
 }
 
 pub fn init_pool(connection_string: &str) -> Pool<ConnectionManager<PgConnection>> {
     let connection_manager = ConnectionManager::<PgConnection>::new(connection_string);
-    let connection_pool = Pool::builder()
-        .max_size(5)
+    Pool::builder()
+        .max_size(15)
         .min_idle(Some(5))
         .build(connection_manager)
-        .expect("Failed to create db connection pool");
-    connection_pool
+        .expect("Failed to create db connection pool")
 }
