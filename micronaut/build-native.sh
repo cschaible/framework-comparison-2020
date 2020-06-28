bbec8ffba80af8a2828ea56a4424dd217fe3fc4f
Christian Schaible <christian.schaible@novatec-gmbh.de>
2020-06-28 21:07:04 +0200

    Link libraries dynamically in the micronaut native build

--- micronaut/build-native.sh
+++ micronaut/build-native.sh
@@ -2,6 +2,6 @@
 ./gradlew clean build
 cd build/libs
 # Add reflection config for PG reflection issue: https://github.com/oracle/graal/issues/2195
-native-image --no-server --static -cp footballmanager-*-all.jar -H:ReflectionConfigurationFiles=../../reflectconfig
+native-image --no-server -cp footballmanager-*-all.jar -H:ReflectionConfigurationFiles=../../reflectconfig
 cd -
 docker build --build-arg "APP_FILE=build/libs/footballmanager" -f Dockerfile.native -t footballmanager-micronaut-native .
